e20b5dc898545d06941811445558601ac78f59e7
Rob Hudson <robhudson@mozilla.com>
2025-05-07 09:12:03 -0700

    Correctly categorize Indian locales (#112)

--- springfield/settings/base.py
+++ springfield/settings/base.py
@@ -163,11 +163,13 @@ LOCALES_BY_REGION = {
     "Americas": ["azz", "cak", "en-CA", "en-US", "es-AR", "es-CL", "es-MX", "gn", "is", "pt-BR", "trs"],
     "Asia Pacific": [
         "bn",
+        "gu-IN",
         "hi-IN",
         "id",
         "ja",
         "kk",
         "km",
+        "kn",
         "ko",
         "lo",
         "ml",
@@ -242,7 +244,7 @@ LOCALES_BY_REGION = {
         "uk",
         "uz",
     ],
-    "Middle East and Africa": ["ach", "af", "ar", "az", "fa", "ff", "gu-IN", "he", "kab", "kn", "skr", "son", "xh"],
+    "Middle East and Africa": ["ach", "af", "ar", "az", "fa", "ff", "he", "kab", "skr", "son", "xh"],
 }
 
 
