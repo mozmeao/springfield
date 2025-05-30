c6c137d8cd0566c01a0205e969d275b86b7b616d
Steve Jalim <stevejalim@mozilla.com>
2025-05-30 17:22:00 +0100

    WIP adding redirects that we're dropping from refractr

--- tests/redirects/map_globalconf.py
+++ tests/redirects/map_globalconf.py
@@ -10,9 +10,32 @@ UA_IOS = {"User-Agent": "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3 like Mac OS X
 
 URLS = flatten(
     (
+        # START https://github.com/mozmeao/springfield/issues/222
+        url_test("/os/", "https://support.mozilla.org/products/firefox-os?redirect_source=firefox-com"),
+        url_test("/desktop/", "/browsers/desktop/"),
+        url_test("/android/", "/browsers/mobile/android/"),
+        url_test("/developer/", "/channel/desktop/developer/"),
+        url_test("/10/", "/features/"),
+        url_test("/independent/", "/features/"),
+        url_test("/hello/", "https://support.mozilla.org/en-US/kb/hello-status?redirect_source=firefox-com"),
+        url_test("/personal/", "/"),
+        url_test("/choose/", "/"),
+        url_test("/switch/", "https://www.mozilla.org/en-GB/firefox/switch/?redirect_source=firefox-com"),
+        url_test("/enterprise/", "/browsers/enterprise/"),
+        url_test("/containers/", "https://www.mozilla.org/firefox/facebookcontainer/?redirect_source=firefox-com"),
+        url_test("/pdx/", "/"),
+        url_test("/pair/", "https://accounts.firefox.com/pair/"),
+        url_test("/join/", "https://www.mozilla.org/firefox/accounts/?redirect_source=join"),
+        url_test("/rejoindre/", "https://www.mozilla.org/firefox/accounts/?redirect_source=join"),
+        url_test("/privacy/", "https://www.mozilla.org/products/?redirect_source=firefox-com"),
+        url_test("/privatsphaere/", "https://www.mozilla.org/products/?redirect_source=firefox-com"),
+        url_test("/nightly/", "/channel/desktop/#nightly`"),
+        url_test("/family/", "https://www.mozilla.org/firefox/family/?utm_medium=referral&utm_source=firefox.com&utm_campaign=firefox-for-families"),
         url_test(
-            "/ignored/placeholder/to/allow/test/setup",
-            "/until-we-rempove-pytest.mark.skip-from-the-test/",
+            "/families/", "https://www.mozilla.org/firefox/family/?utm_medium=referral&utm_source=firefox.com&utm_campaign=firefox-for-families"
         ),
+        url_test("/family/some/path", "https://www.mozilla.org/firefox/family/"),
+        url_test("/families/some/path", "https://www.mozilla.org/firefox/family/"),
+        # END https://github.com/mozmeao/springfield/issues/222
     )
 )
