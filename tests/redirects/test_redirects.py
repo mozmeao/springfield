17b8df5efdb94c83402dc31ed4985f854738e403
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2025-06-08 03:02:31 +0200

    Simplify redirects tests

--- tests/redirects/test_redirects.py
+++ tests/redirects/test_redirects.py
@@ -2,44 +2,22 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-"""Test redirects from the global.conf file."""
+"""Test redirects from all apps."""
 
 from operator import itemgetter
 
 import pytest
 
 from .base import assert_valid_url
-from .map_301 import URLS as REDIRECT_URLS
-from .map_globalconf import URLS as GLOBAL_URLS
-from .map_htaccess import URLS as HTA_URLS
+from .map_all_redirects import URLS as REDIRECT_URLS
 from .map_locales import URLS as LOCALE_URLS
 
 
-@pytest.mark.skip("Disabled until we have redirects to test")
 @pytest.mark.headless
 @pytest.mark.nondestructive
 @pytest.mark.django_db
 @pytest.mark.parametrize("url", REDIRECT_URLS, ids=itemgetter("url"))
-def test_301_url(url, base_url):
-    url["base_url"] = base_url
-    assert_valid_url(**url)
-
-
-@pytest.mark.headless
-@pytest.mark.nondestructive
-@pytest.mark.django_db
-@pytest.mark.parametrize("url", GLOBAL_URLS, ids=itemgetter("url"))
-def test_global_conf_url(url, base_url):
-    url["base_url"] = base_url
-    assert_valid_url(**url)
-
-
-@pytest.mark.skip("Disabled until we have redirects to test")
-@pytest.mark.headless
-@pytest.mark.nondestructive
-@pytest.mark.django_db
-@pytest.mark.parametrize("url", HTA_URLS, ids=itemgetter("url"))
-def test_htaccess_url(url, base_url):
+def test_redirect_url(url, base_url):
     url["base_url"] = base_url
     assert_valid_url(**url)
 
