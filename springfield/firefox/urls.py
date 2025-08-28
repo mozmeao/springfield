67eef5415cbfecc82f9c6d52f38484fd8cee2e24
Issam Mani <issamouu69@gmail.com>
2025-08-28 18:26:52 +0200

    fix: add testflight link back

--- springfield/firefox/urls.py
+++ springfield/firefox/urls.py
@@ -31,6 +31,7 @@ urlpatterns = (
     page("channel/desktop/developer/", "firefox/developer/index.html", ftl_files=["firefox/developer"]),
     page("channel/android/", "firefox/channel/android.html", ftl_files=["firefox/channel"]),
     page("channel/ios/", "firefox/channel/ios.html", ftl_files=["firefox/channel"]),
+    path("channel/ios/testflight/", views.ios_testflight, name="firefox.ios.testflight"),
     # TODO: Enable this once we have the final video asset for the hero section
     # page("browsers/mobile/ios/summarizer/", "firefox/browsers/mobile/ios-summarizer.html", ftl_files=["firefox/browsers/mobile/ios-summarizer", "firefox/channel"]),
     page("browsers/enterprise/", "firefox/enterprise/index.html", ftl_files=["firefox/enterprise"]),
