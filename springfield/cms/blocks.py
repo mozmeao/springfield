6be82839ecb70e8f792fd262d7d16097a6f1ef59
Dmitriy Chukhin <6025153+dchukhin@users.noreply.github.com>
2025-09-23 15:05:07 -0400

    fix lint error from merge

--- springfield/cms/blocks.py
+++ springfield/cms/blocks.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from django.templatetags.static import static
-
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
