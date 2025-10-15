006dfcbf85c86407ffb0c55dd852ce4e140c1460
Mariana Bedran Lesche <maribedran@gmail.com>
2025-10-15 12:49:08 -0300

    Ruff

--- springfield/cms/models/base.py
+++ springfield/cms/models/base.py
@@ -69,7 +69,7 @@ class AbstractSpringfieldCMSPage(WagtailBasePage):
         # can swap that for our Fluent-compatible rendering method
         template = self.get_template(request, *args, **kwargs)
         context = self.get_context(request, *args, **kwargs)
-        # If we need any special Fluent files to accompany CMS content, spec them as the ftl_files attribute (a List) 
+        # If we need any special Fluent files to accompany CMS content, spec them as the ftl_files attribute (a List)
         # on the page class. If None is specced, we default to what's in settings.FLUENT_DEFAULT_FILES
         return l10n_utils.render(request, template, context, ftl_files=self.ftl_files)
 
