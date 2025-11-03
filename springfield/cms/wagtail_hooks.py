f30880730417ce4429ee10c1dbfe04d5d4daad11
Mariana Bedran Lesche <maribedran@gmail.com>
2025-11-03 11:47:08 -0300

    Fix richtect JS path

--- springfield/cms/wagtail_hooks.py
+++ springfield/cms/wagtail_hooks.py
@@ -4,7 +4,6 @@
 
 from uuid import uuid4
 
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
@@ -124,7 +123,7 @@ def register_fxa(features):
     features.register_editor_plugin(
         "draftail",
         feature,
-        draftail_features.EntityFeature(control, js=[staticfiles_storage.url("js/wagtailadmin-fxa.js")]),
+        draftail_features.EntityFeature(control, js="js/wagtailadmin-fxa.js"),
     )
 
     db_conversion = {
