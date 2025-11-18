eb7c57c9d37a1748d3a85db51102484582eed372
Luciano Ratamero <luciano@ratamero.com>
2025-11-18 05:41:42 -0300

    Add js needed for mobile navigation menus

---
+++ media/js/cms/flare-navigation.js
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+(function () {
+    'use strict';
+    const buttonEl = document.querySelector('.fl-hamburger');
+
+    buttonEl.addEventListener('click', function (e) {
+        e.preventDefault();
+        const mobileNavIsOpen = e.target.classList.contains('is-open');
+        const elements = [e.target, document.querySelector('.fl-nav')];
+
+        if (mobileNavIsOpen) {
+            elements.forEach(function (el) {
+                el.classList.remove('is-open');
+            });
+            document.body.classList.remove('modal-open');
+        } else {
+            elements.forEach(function (el) {
+                el.classList.add('is-open');
+            });
+            document.body.classList.add('modal-open');
+        }
+    });
+})();
