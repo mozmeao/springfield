0656b0143cdd078622a76d8ec72d9a81ce8c96d5
luciano ratamero <luciano@ratamero.com>
2026-01-08 18:07:33 -0300

    2026 - 1 -WT-369: Footer styles (#896)
    * Upgrade Pattern Library
    
    * Don't use flare 2026 template where styles aren't available
    
    * Create pattern library pages for existing 2026 designs
    
    * Implement Firefox logo rich text feature
    
    * Add inline Firefox Logo typography examples to pattern library
    
    * Add 2026 inline Firefox Logo to pattern library
    
    * Add new icons and use a single icon set for flare designs
    
    * Rename icon CSS file to flare26-icon
    
    * Create Card Gallery component and CMS block
    
    * Create Pre Footer CTA component
    
    * Create Pre Footer CTA snippet on CMS
    
    * Create 2026 Kit Banner block
    
    * Add Kit Banner block to Home page
    
    * Upgrade Pattern Library
    
    * Don't use flare 2026 template where styles aren't available
    
    * Create pattern library pages for existing 2026 designs
    
    * Implement Firefox logo rich text feature
    
    * Add inline Firefox Logo typography examples to pattern library
    
    * Add 2026 inline Firefox Logo to pattern library
    
    * Add new icons and use a single icon set for flare designs
    
    * Rename icon CSS file to flare26-icon
    
    * Create Card Gallery component and CMS block
    
    * Create Pre Footer CTA component
    
    * Create Pre Footer CTA snippet on CMS
    
    * Create 2026 Kit Banner block
    
    * Add Kit Banner block to Home page
    
    * carousel progress indicator CSS
    
    * Add embla-carousel dependency
    
    * Implement carousel animation using embla
    
    * Add Flare 2026 navigation styles
    
    * Add Fluent context to pattern librabry
    
    * Update WNP default type styles in Pattern Library, remove max-width on body
    
    * update vertical alignment on FF heading logo
    
    * minor card gallery CSS revisions
    
    * prepping cards for themes and dark mode, but also disabling current dark mode work
    
    * kit resolution fix, relative positioning adjustments
    
    * image size revision, ghost button support
    
    * fixing padding issue, so the active image takes up the whole width
    
    * scope theme variables to header, so they don't bleed into other compoenents
    
    * hide "unsupported" message on header download button
    
    * 2026 - 7 - Make dynamic card grid more balanced (#858)
    
    * Add balance to sticker card layout
    
    * card grid refactor to align with figma mobile/template layouts
    
    * Fix home banner style regression
    
    The ghost button class was incorrectly applied to this button, which became apparent when ghost button styles were added to this branch
    
    * Add superheading text to homepage cards
    
    * Adjust card sizing/spacing/order
    
    * Whitespace fix
    
    ---------
    
    Co-authored-by: Kasey Kelly <kasey@servee.com>
    Co-authored-by: Mariana Bedran Lesche <maribedran@gmail.com>
    
    * fixing homepage spacing issues
    
    * Fix issue on header button clicks on mobile
    
    * Add svg for hamburger menu button
    
    * Implement nav mobile buttons like figma
    
    * Change copy on header download button for flare26
    
    * fixing card grid regression
    
    * legacy nav and footer styles
    
    * Update content of the flare26 footer
    
    * Add new css for the flare26 footer
    
    * Add footer's  lang switcher
    
    * Refactor flare26 footer into smaller includes
    
    * Use flow-relative css instead of top/right/bottom/left
    
    * Add venv to ignores eslint files
    
    * Use flow-relative css on lang switcher css too
    
    * Update navigation ftl strings
    
    * Add missing ftl string to footer
    
    * Remove unused footer styles
    
    * Fix language switcher min width
    
    * Fix keyboard interaction when shift+tab is pressed for leaving a menu
    
    * Fix keyboard + mouse interaction on nav, when hovering after using keyboard
    
    * Implement nav light/dark modes with class overrides
    
    * legacy rework
    
    * some unnecessary calcs removed
    
    * theme refactor
    
    ---------
    
    Co-authored-by: Mariana Bedran Lesche <maribedran@gmail.com>
    Co-authored-by: Kasey Kelly <kasey@servee.com>
    Co-authored-by: Jo Sprague <github@iamlocaljo.com>

--- media/js/cms/flare-navigation.es6.js
+++ media/js/cms/flare-navigation.es6.js
@@ -62,6 +62,9 @@ import { createFocusTrap } from 'focus-trap';
     // mouse is being used
     menuCategories.forEach(function (category) {
         category.addEventListener('mouseover', function () {
+            menuCategories.forEach(function (category) {
+                category.classList.remove('is-active');
+            });
             category.classList.add('is-active');
         });
         category.addEventListener('mouseout', function () {
@@ -73,10 +76,18 @@ import { createFocusTrap } from 'focus-trap';
 
     // keyboard is being used
     menuTitles.forEach(function (title) {
+        // when focusing a menu title, close all menus
+        title.addEventListener('focus', function () {
+            menuCategories.forEach(function (category) {
+                category.classList.remove('is-active');
+            });
+        });
+
         // when leaving the last link of a menu, close all menus
         const menuLinks = title
             .closest('.fl-menu-category')
             .querySelectorAll('a');
+
         menuLinks[menuLinks.length - 1].addEventListener(
             'keydown',
             function (event) {
