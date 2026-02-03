3f7935c4019c8628d4b85976aed554fe986e4303
Jingwen Zhu <42974891+wen-2018@users.noreply.github.com>
2026-02-03 06:08:43 -0400

    new download page (#919)
    * new download page
    
    * Move experiment from home to /landing/get
    
    * Add US geo restriction for experiment, reduce locale restriction to en-US only
    
    * Update confirmed traffic split
    
    ---------
    
    Co-authored-by: maureenlholland <19650432+maureenlholland@users.noreply.github.com>

---
+++ media/js/firefox/experiment-home.es6.js
@@ -0,0 +1,76 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import TrafficCop from '@mozmeao/trafficcop';
+import { isApprovedToRun } from '../base/experiment-utils.es6';
+import {
+    meetsExperimentCriteria,
+    experimentCookieID
+} from './experiment-meets-criteria.es6';
+
+const href = window.location.href;
+if (typeof window.dataLayer === 'undefined') {
+    window.dataLayer = [];
+}
+
+const experimentName = 'download-privacy';
+
+/**
+ * Sets a cookie to remember which experiment variation has been seen.
+ * @param {Object} traffic cop config
+ */
+function setVariationCookie(exp) {
+    // set cookie to expire in 24 hours
+    const date = new Date();
+    date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+    const expires = date.toUTCString();
+
+    window.Mozilla.Cookies.setItem(
+        exp.id,
+        exp.chosenVariation,
+        expires,
+        undefined,
+        undefined,
+        false,
+        'lax'
+    );
+}
+
+const init = () => {
+    if (href.indexOf(`experiment=${experimentName}&variation=control`) !== -1) {
+        window.dataLayer.push({
+            event: 'experiment_view',
+            id: experimentCookieID,
+            variant: 'control'
+        });
+    } else if (
+        href.indexOf(`experiment=${experimentName}&variation=treatment`) !== -1
+    ) {
+        window.dataLayer.push({
+            event: 'experiment_view',
+            id: experimentCookieID,
+            variant: 'treatment'
+        });
+    } else if (TrafficCop) {
+        if (isApprovedToRun()) {
+            // NOTE: experiment & variation params are needed for Stub Attribution, v param for VariationTemplateView
+            const cop = new TrafficCop({
+                id: experimentCookieID,
+                variations: {
+                    [`experiment=${experimentName}&variation=control`]: 50,
+                    [`experiment=${experimentName}&variation=treatment&v=treatment`]: 50
+                }
+            });
+            cop.init();
+
+            setVariationCookie(cop);
+        }
+    }
+};
+
+if (meetsExperimentCriteria()) {
+    init();
+}
