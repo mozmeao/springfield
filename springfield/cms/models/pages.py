4d0b1380070a11165075eb31bffd0f522dab40e4
Luciano Ratamero <luciano@ratamero.com>
2026-02-03 17:57:15 -0300

    Fix article index page filtering

--- springfield/cms/models/pages.py
+++ springfield/cms/models/pages.py
@@ -307,7 +307,11 @@ class ArticleIndexPage(UTMParamsMixin, AbstractSpringfieldCMSPage):
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request)
 
-        all_articles = [page.specific for page in self.get_children().live().public().order_by("-first_published_at")]
+        all_articles = [
+            page.specific
+            for page in self.get_children().live().public().order_by("-first_published_at")
+            if isinstance(page.specific, ArticleDetailPage)
+        ]
 
         featured_articles = [page for page in all_articles if isinstance(page, ArticleDetailPage) and page.featured]
         list_articles = [page for page in all_articles if isinstance(page, ArticleDetailPage) and not page.featured]
