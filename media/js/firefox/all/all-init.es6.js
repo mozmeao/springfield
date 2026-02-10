d1258f22e0af3023c6ba57412b1bacd4ae9d9bdb
maureenlholland <19650432+maureenlholland@users.noreply.github.com>
2026-02-10 11:26:26 +0000

    /all fixes
    Remove preventScroll on focus (fixes #946)
    Update StubAttribution only when download buttons appear (fixes #959)

--- media/js/firefox/all/all-init.es6.js
+++ media/js/firefox/all/all-init.es6.js
@@ -4,6 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+import StubAttributionConsent from '../../base/stub-attribution/stub-attribution-consent.es6';
 import TrackProductDownload from '../../base/datalayer-productdownload.es6';
 import MzpModal from '@mozilla-protocol/core/protocol/js/modal';
 
@@ -20,7 +21,10 @@ import MzpModal from '@mozilla-protocol/core/protocol/js/modal';
         const installerHelpIcon = document.querySelectorAll(
             '.icon-installer-help'
         );
-        const downloadButtons = document.querySelectorAll('.download-link');
+        // We don't want to include nav download button
+        const downloadButtons = document.querySelectorAll(
+            '#outer-wrapper .download-link'
+        );
 
         function showHelpModal(modalContent, modalTitle, eventLabel) {
             MzpModal.createModal(this, modalContent, {
@@ -73,8 +77,12 @@ import MzpModal from '@mozilla-protocol/core/protocol/js/modal';
             }
         }
 
-        // event tracking for GA4
-        if (downloadButtons) {
+        // init stub attribution & event tracking for GA4
+        if (downloadButtons && downloadButtons.length > 0) {
+            // We cannot rely on DOM ready as this section is partially fetched
+            // This flow is scheduled for refactoring: https://github.com/mozmeao/springfield/issues/258
+            StubAttributionConsent.init();
+
             for (let i = 0; i < downloadButtons.length; ++i) {
                 const downloadButton = downloadButtons[i];
                 downloadButton.addEventListener(
@@ -128,7 +136,7 @@ import MzpModal from '@mozilla-protocol/core/protocol/js/modal';
                     '.c-step-name:not(.t-step-disabled)'
                 );
                 const targetHeader = activeHeaders[activeHeaders.length - 1];
-                targetHeader.focus({ preventScroll: true });
+                targetHeader.focus();
             })
             .catch((error) => {
                 throw new Error(
