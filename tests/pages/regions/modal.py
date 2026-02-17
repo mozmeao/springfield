1b503adf4ea338fd515e1b12b7754b320a6bd17f
Luciano Ratamero <luciano@ratamero.com>
2026-02-17 19:24:40 -0300

    Remove smoke tests related to modals ahead of flare26's launch

--- tests/pages/regions/modal.py
+++ tests/pages/regions/modal.py
@@ -23,32 +23,3 @@ class Modal(BaseRegion):
 
     def displays(self, selector):
         return self.is_element_displayed(*selector)
-
-
-class ModalProtocol(BaseRegion):
-    _protocol_root_locator = (By.CLASS_NAME, "mzp-c-modal")
-    _protocol_close_locator = (By.CLASS_NAME, "mzp-c-modal-button-close")
-    _flare26_root_locator = (By.CLASS_NAME, "fl-dialog")
-    _flare26_close_locator = (By.CLASS_NAME, "fl-dialog-close-button")
-
-    def close(self):
-        modal = self.selenium.find_element(*self._protocol_root_locator)
-        if not modal:
-            modal = self.selenium.find_element(*self._flare26_root_locator)
-
-        close = self.selenium.find_element(*self._protocol_close_locator)
-        if not close:
-            close = self.selenium.find_element(*self._flare26_close_locator)
-
-        close.click()
-        self.wait.until(expected.staleness_of(modal))
-
-    @property
-    def is_displayed(self):
-        is_displayed = self.page.is_element_displayed(*self._protocol_root_locator)
-        if not is_displayed:
-            is_displayed = self.page.is_element_displayed(*self._flare26_root_locator)
-        return is_displayed
-
-    def displays(self, selector):
-        return self.is_element_displayed(*selector)
