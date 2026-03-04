23c8657909873074753361232a3c0bcccd0d93c0
Luciano Ratamero <luciano@ratamero.com>
2026-03-04 15:41:34 -0300

    Fix navigation specs, enabling it for both flare26 and protocol

--- tests/playwright/specs/navigation.spec.js
+++ tests/playwright/specs/navigation.spec.js
@@ -8,108 +8,121 @@
 
 const { test, expect } = require('@playwright/test');
 const openPage = require('../scripts/open-page');
-const url = '/en-US/';
+const languages = ['en-US', 'sv-SE'];
 
-test.describe(
-    `${url} navigation (desktop)`,
-    {
-        tag: '@firefox'
-    },
-    () => {
-        test.beforeEach(async ({ page, browserName }) => {
-            await openPage(url, page, browserName);
-        });
-
-        test.skip('Navigation menu hover', async ({ page }) => {
-            const resourceLink = page.getByTestId('navigation-link-resources');
-            const resourceMenu = page.getByTestId('navigation-menu-resources');
-
-            // Hover over Firefox link
-            await resourceLink.hover();
-            await expect(resourceMenu).toBeVisible();
-        });
-
-        test.skip('Navigation link click', async ({ page }) => {
-            const resourceLink = page.getByTestId('navigation-link-resources');
-            const resourceMenu = page.getByTestId('navigation-menu-resources');
-            const firefoxMenuLink = page.getByTestId(
-                'navigation-menu-link-data-protection'
-            );
-
-            // Hover over Firefox link
-            await resourceLink.hover();
-            await expect(resourceMenu).toBeVisible();
-            await expect(firefoxMenuLink).toBeVisible();
-
-            // Click data protection link
-            await firefoxMenuLink.click();
-            await page.waitForURL('/en-US/user-privacy/', {
-                waitUntil: 'commit'
+languages.forEach((lang) => {
+    test.describe(
+        `${lang} navigation (desktop)`,
+        {
+            tag: '@firefox'
+        },
+        () => {
+            test.beforeEach(async ({ page, browserName }) => {
+                await openPage(`/${lang}/`, page, browserName);
             });
 
-            // Assert Firefox menu is closed after navigation
-            await expect(resourceMenu).not.toBeVisible();
-        });
-    }
-);
+            test('Navigation menu hover', async ({ page }) => {
+                const resourceLink = page.getByTestId(
+                    'navigation-link-resources'
+                );
+                const resourceMenu = page.getByTestId(
+                    'navigation-menu-resources'
+                );
 
-test.describe(
-    `${url} navigation (mobile)`,
-    {
-        tag: '@firefox'
-    },
-    () => {
-        test.use({ viewport: { width: 360, height: 780 } });
-
-        test.beforeEach(async ({ page, browserName }) => {
-            await openPage(url, page, browserName);
-        });
-
-        test.skip('Navigation open / close click', async ({ page }) => {
-            const navigationMenuButton = page.getByTestId(
-                'navigation-menu-button'
-            );
-            const navigationMenuItems = page.getByTestId(
-                'navigation-menu-items'
-            );
-            const browserMenu = page.getByTestId('navigation-menu-browser');
-
-            // Open navigation menu
-            await navigationMenuButton.click();
-            await expect(navigationMenuItems).toBeVisible();
-
-            // Browser menu should be open by default
-            await expect(browserMenu).toBeVisible();
-
-            // Close navigation menu
-            await navigationMenuButton.click();
-            await expect(navigationMenuItems).not.toBeVisible();
-        });
-
-        test.skip('Navigation link click', async ({ page }) => {
-            const navigationMenuButton = page.getByTestId(
-                'navigation-menu-button'
-            );
-            const navigationMenuItems = page.getByTestId(
-                'navigation-menu-items'
-            );
-            const firefoxMenuLink = page.getByTestId(
-                'navigation-menu-link-mobile'
-            );
-
-            // Open navigation menu
-            await navigationMenuButton.click();
-            await expect(navigationMenuItems).toBeVisible();
-            await expect(firefoxMenuLink).toBeVisible();
-
-            // Click mobile link
-            await firefoxMenuLink.click();
-            await page.waitForURL('/en-US/browsers/mobile/', {
-                waitUntil: 'commit'
+                // Hover over Firefox link
+                await resourceLink.hover();
+                await expect(resourceMenu).toBeVisible();
             });
 
-            // Assert nav menu is closed again after navigation
-            await expect(navigationMenuItems).not.toBeVisible();
-        });
-    }
-);
+            test('Navigation link click', async ({ page }) => {
+                const resourceLink = page.getByTestId(
+                    'navigation-link-resources'
+                );
+                const resourceMenu = page.getByTestId(
+                    'navigation-menu-resources'
+                );
+                const firefoxMenuLink = page.getByTestId(
+                    'navigation-menu-link-data-protection'
+                );
+
+                // Hover over Firefox link
+                await resourceLink.hover();
+                await expect(resourceMenu).toBeVisible();
+                await expect(firefoxMenuLink).toBeVisible();
+
+                // Click data protection link
+                await firefoxMenuLink.click();
+                await page.waitForURL('/en-US/user-privacy/', {
+                    waitUntil: 'commit'
+                });
+
+                // Assert Firefox menu is closed after navigation
+                await expect(resourceMenu).not.toBeVisible();
+            });
+        }
+    );
+
+    test.describe(
+        `${lang} navigation (mobile)`,
+        {
+            tag: '@firefox'
+        },
+        () => {
+            test.use({ viewport: { width: 360, height: 780 } });
+
+            test.beforeEach(async ({ page, browserName }) => {
+                await openPage(`/${lang}/`, page, browserName);
+            });
+
+            test('Navigation open / close click', async ({ page }) => {
+                const navigationMenuButton = page.getByTestId(
+                    'navigation-menu-button'
+                );
+                const navigationMenuItems = page.getByTestId(
+                    'navigation-menu-items'
+                );
+                const browserMenu = page.getByTestId(
+                    'navigation-menu-resources'
+                );
+
+                // Open navigation menu
+                await navigationMenuButton.click();
+                await expect(navigationMenuItems).toBeVisible();
+
+                // Browser menu should be open by default
+                await expect(browserMenu).toBeVisible();
+
+                // Close navigation menu
+                await navigationMenuButton.click();
+                await expect(navigationMenuItems).not.toBeVisible();
+            });
+
+            test('Navigation link click', async ({ page }) => {
+                const navigationMenuButton = page.getByTestId(
+                    'navigation-menu-button'
+                );
+                const navigationMenuItems = page.getByTestId(
+                    'navigation-menu-items'
+                );
+                const firefoxMenuLink = page.getByTestId(
+                    'navigation-menu-link-data-protection'
+                );
+
+                // Open navigation menu
+                await navigationMenuButton.click();
+                await expect(navigationMenuItems).toBeVisible();
+                await expect(firefoxMenuLink).toBeVisible();
+
+                // Click mobile link
+                await firefoxMenuLink.click();
+                // this page only exists in the en-US locale
+                await page.waitForURL(`/en-US/user-privacy/`, {
+                    waitUntil: 'commit'
+                });
+
+                // Assert nav menu is closed again after navigation
+                await expect(navigationMenuItems).not.toBeVisible();
+            });
+        }
+    );
+});
