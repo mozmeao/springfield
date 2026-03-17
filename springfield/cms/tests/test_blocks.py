e5a61ff92a6b22cb6c3c31fca4fd50b8610c9828
Mariana Bedran Lesche <mariana@lincolnloop.com>
2026-03-17 18:00:33 -0300

    [WT-889] What's New page 2026 (#1141)
    * Create What's New Page 2026
    
    * Add media content block to 2026 pages
    
    * Add layout settings to 2026 intro block
    
    * Add phone QR variant to banner snippet
    
    * Fix localized snippet chooser block
    
    * Add other media types to intro block
    
    * Add optional sticker to outlined card
    
    * WNP 2026 CSS revisions
    
    * WNP style fixes
    
    * footer logo updated
    
    * intro revisions
    
    * revert featured image revision
    
    * fixing outline card heading size
    
    * Fix article index page tests
    
    ---------
    
    Co-authored-by: luciano ratamero <luciano@ratamero.com>
    Co-authored-by: Kasey Kelly <kasey@servee.com>

--- springfield/cms/tests/test_blocks.py
+++ springfield/cms/tests/test_blocks.py
@@ -49,6 +49,8 @@ from springfield.cms.fixtures.card_gallery_2026_fixtures import get_card_gallery
 from springfield.cms.fixtures.cards_2026_fixtures import (
     get_illustration_card_2026_variants,
     get_illustration_cards_2026_test_page,
+    get_outlined_card_2026_variants,
+    get_outlined_cards_2026_test_page,
     get_step_card_2026_variants,
     get_step_cards_2026_test_page,
     get_sticker_card_2026_variants,
@@ -2314,17 +2316,59 @@ def test_intro_2026_block(index_page, placeholder_images, rf):
 
         for index, variant in enumerate(variants):
             intro_el = intro_divs[index]
-            heading_text = BeautifulSoup(variant["value"]["heading"]["heading_text"], "html.parser").get_text()
+            value = variant["value"]
+            intro_classes = intro_el.get("class", [])
+
+            # Heading
+            heading_text = BeautifulSoup(value["heading"]["heading_text"], "html.parser").get_text()
             heading = intro_el.find(class_="fl-heading")
             assert heading and heading_text in heading.get_text()
 
-            media = variant["value"].get("media")
+            # Settings: layout
+            layout = value["settings"]["layout"]
+            if layout == "vertical":
+                assert "fl-intro-vertical" in intro_classes
+            elif layout == "right" and value["media"]:
+                assert "fl-intro-media-right" in intro_classes
+            elif layout == "left" and value["media"]:
+                assert "fl-intro-media-left" in intro_classes
+
+            # Settings: slim
+            if value["settings"]["slim"]:
+                assert "is-slim" in intro_classes
+            else:
+                assert "is-slim" not in intro_classes
+
+            # Settings: anchor_id
+            anchor_id = value["settings"]["anchor_id"]
+            if anchor_id:
+                assert intro_el.get("id") == anchor_id
+            else:
+                assert not intro_el.get("id")
+
+            # Media
+            media = value.get("media")
             if media:
                 media_block = media[0]
+                media_el = intro_el.find("div", class_="fl-intro-media")
+                assert media_el
                 if media_block["type"] == "image":
-                    assert intro_el.find("div", class_="fl-intro-media").find("img")
+                    assert_image_variants_attributes(
+                        images_element=media_el,
+                        images_value=media_block["value"],
+                        sizes="(min-width: 1200px) 934px, (min-width: 600px) 50vw, 100vw",
+                        widths="width-{200,400,600,800,1000,1200,1400,1600,1800,2000}",
+                    )
                 elif media_block["type"] == "video":
-                    assert intro_el.find("div", class_="fl-video")
+                    assert_video_attributes(intro_el.find("div", class_="fl-video"), media_block)
+                elif media_block["type"] == "animation":
+                    assert_animation_attributes(intro_el.find("div", class_="fl-video"), media_block)
+                elif media_block["type"] == "qr_code":
+                    qr_div = media_el.find("div", class_="fl-media-qr-code")
+                    assert qr_div
+                    assert qr_div.find("div", class_="fl-qr-code").find("svg")
+                    if media_block["value"].get("background"):
+                        assert qr_div.find("img")
             else:
                 assert not intro_el.find("div", class_="fl-intro-media")
 
@@ -2511,6 +2555,71 @@ def test_step_cards_2026_block(index_page, placeholder_images, rf):
 
             # Buttons
             for button_data in variant["value"]["buttons"]:
+                if button_data["type"] == "button":
+                    button_el = card_el.find("a", class_="fl-button")
+                    assert_button_attributes(
+                        button_element=button_el,
+                        button_data=button_data,
+                        context=context,
+                    )
+
+
+def test_outlined_cards_2026_block(index_page, placeholder_images, rf):
+    variants = get_outlined_card_2026_variants()
+    page = get_outlined_cards_2026_test_page()
+
+    request = rf.get(page.get_full_url())
+    response = page.serve(request)
+    assert response.status_code == 200
+
+    context = page.get_context(request)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    upper = soup.find("div", class_="fl-split-page-upper")
+    lower = soup.find("div", class_="fl-split-page-lower")
+    assert upper and lower
+
+    for region in [upper, lower]:
+        sections = region.find_all("section", class_="fl-section")
+        assert len(sections) == 2
+
+        # First section has 3 cards, second has 4
+        assert len(sections[0].find_all("article", class_="fl-card")) == 3
+        assert len(sections[1].find_all("article", class_="fl-card")) == 4
+
+        # Verify card content in the 4-card section
+        cards = sections[1].find_all("article", class_="fl-card")
+        for i, variant in enumerate(variants):
+            card_el = cards[i]
+            value = variant["value"]
+
+            # Headline
+            headline_text = BeautifulSoup(value["headline"], "html.parser").get_text()
+            heading = card_el.find(class_="fl-heading")
+            assert heading and headline_text in heading.get_text()
+
+            # Expand link
+            if value["settings"].get("expand_link"):
+                assert "fl-card-expand-link" in card_el.get("class", [])
+            else:
+                assert "fl-card-expand-link" not in card_el.get("class", [])
+
+            # Content body
+            content_text = BeautifulSoup(value["content"], "html.parser").get_text()
+            body = card_el.find(class_="fl-body")
+            assert body and content_text in body.get_text()
+
+            # Sticker (optional - present when sticker has a non-null image)
+            if value.get("sticker", {}).get("image"):
+                sticker_el = card_el.find("div", class_="fl-card-sticker")
+                assert sticker_el
+                assert_image_variants_attributes(
+                    images_element=sticker_el,
+                    images_value=value["sticker"],
+                )
+
+            # Buttons
+            for button_data in value["buttons"]:
                 if button_data["type"] == "button":
                     button_el = card_el.find("a", class_="fl-button")
                     assert_button_attributes(
