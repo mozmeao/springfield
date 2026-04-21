1c31eb8eb6506877b8627d2669f679506043649f
Luciano Ratamero <luciano@ratamero.com>
2026-04-21 18:11:57 -0300

    Unify new.es6.js and flare26.es6.js into one file

--- media/js/cms/flare26.es6.js
+++ media/js/cms/flare26.es6.js
@@ -4,14 +4,49 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import setupCarousels from './flare26-carousel.es6';
-import setupScrollingCardGrid from './flare26-scrolling-card-grid.es6';
-import setupCopyToClipboardButtons from './flare26-copy-to-clipboard.es6';
-import setupFirefoxVersionConditionalDisplay from './flare26-firefox-version.es6';
-import setupSlidingCarousels from './flare26-sliding-carousel.es6';
+import setupAnimations from './components/flare26-animations.es6';
+import setupCarousels from './components/flare26-carousel.es6';
+import setupCopyToClipboardButtons from './components/flare26-copy-to-clipboard.es6';
+import setupDialogs, { initDialogs } from './components/flare26-dialogs.es6';
+import setupDownloadDropdown from './components/flare26-download-dropdown.es6';
+import setupFirefoxVersionConditionalDisplay from './components/flare26-firefox-version.es6';
+import setupNewsletter from './components/flare26-newsletter.es6';
+import setupNotificationClose from './components/flare26-notification-close.es6';
+import setupQRCodeSnippet from './components/flare26-qr-code-snippet.es6';
+import setupScrollingCardGrid from './components/flare26-scrolling-card-grid.es6';
+import setupSlidingCarousels from './components/flare26-sliding-carousel.es6';
+import setupTopicListSidebar from './components/flare26-topic-list-sidebar.es6';
+import setupTypewriter, {
+    typewriter
+} from './components/flare26-typewriter.es6';
+import setupVideo from './components/flare26-video.es6';
 
-setupCarousels();
-setupScrollingCardGrid();
-setupCopyToClipboardButtons();
-setupFirefoxVersionConditionalDisplay();
-setupSlidingCarousels();
+// Create namespace
+if (typeof window.cms === 'undefined') {
+    window.cms = {};
+}
+
+window.cms.Flare26 = { typewriter, initDialogs };
+
+function setupComponents() {
+    setupNewsletter();
+    setupNotificationClose();
+    setupVideo();
+    setupAnimations();
+    setupDownloadDropdown();
+    setupQRCodeSnippet();
+    setupTopicListSidebar();
+    setupTypewriter();
+    setupDialogs();
+    setupCarousels();
+    setupScrollingCardGrid();
+    setupCopyToClipboardButtons();
+    setupFirefoxVersionConditionalDisplay();
+    setupSlidingCarousels();
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', setupComponents);
+} else {
+    setupComponents();
+}
