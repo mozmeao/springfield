2654e328bd8a3af48f443aa2b845e1e398385e69
Dan Brown <dbrown@mozilla.com>
2026-04-23 11:27:04 -0400

    adding update_instructions to CMS for smart window page (#1297)

--- springfield/cms/models/pages.py
+++ springfield/cms/models/pages.py
@@ -1199,6 +1199,7 @@ class SmartWindowPage(UTMParamsMixin, AbstractSpringfieldCMSPage):
                 FieldPanel("nav_download_button_uid"),
                 FieldPanel("intro_download_button_uid"),
                 FieldPanel("update_button_label"),
+                FieldPanel("update_instructions"),
                 FieldPanel("update_link"),
                 FieldPanel("copy_to_clipboard_label"),
                 FieldPanel("copy_success_label"),
