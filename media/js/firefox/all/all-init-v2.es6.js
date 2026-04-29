882c8e3a24e1fce7f45839d30d71a507e898ad20
maureenlholland <19650432+maureenlholland@users.noreply.github.com>
2026-04-29 15:41:50 +0100

    Duplicate all-init JS for use with new Download Attribution JS

---
+++ media/js/firefox/all/all-init-v2.es6.js
@@ -0,0 +1,190 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import StubAttributionConsent from '../../base/stub-attribution/stub-attribution-consent.es6';
+import TrackProductDownload from '../../base/datalayer-productdownload.es6';
+import MzpModal from '@mozilla-protocol/core/protocol/js/modal';
+
+(function (Mozilla) {
+    // Getter function since outerHTML replacement in fetchContent breaks existing references
+    function getPartialTargetElement() {
+        return document.getElementById('partial-target');
+    }
+
+    function setUpPartialContentListeners() {
+        const browserHelpContent = document.getElementById('browser-help');
+        const browserHelpIcon = document.getElementById('icon-browser-help');
+        const installerHelpContent = document.getElementById('installer-help');
+        const installerHelpIcon = document.querySelectorAll(
+            '.icon-installer-help'
+        );
+        // We don't want to include nav download button
+        const downloadButtons = document.querySelectorAll(
+            '#outer-wrapper .download-link'
+        );
+
+        function showHelpModal(modalContent, modalTitle, eventLabel) {
+            MzpModal.createModal(this, modalContent, {
+                title: modalTitle,
+                className: 'help-modal'
+            });
+
+            // GA4
+            window.dataLayer.push({
+                event: 'widget_action',
+                type: 'modal',
+                action: 'open',
+                name: eventLabel
+            });
+        }
+
+        // Browser help modal.
+        if (browserHelpIcon) {
+            browserHelpIcon.addEventListener(
+                'click',
+                function (e) {
+                    e.preventDefault();
+                    showHelpModal.call(
+                        this,
+                        browserHelpContent,
+                        browserHelpIcon.textContent,
+                        'Get Browser Help'
+                    );
+                },
+                false
+            );
+        }
+
+        // Installer help modal.
+        if (installerHelpIcon) {
+            for (let i = 0; i < installerHelpIcon.length; i++) {
+                installerHelpIcon[i].addEventListener(
+                    'click',
+                    function (e) {
+                        e.preventDefault();
+                        showHelpModal.call(
+                            this,
+                            installerHelpContent,
+                            e.target.textContent,
+                            'Get Installer Help'
+                        );
+                    },
+                    false
+                );
+            }
+        }
+
+        if (
+            window.cms &&
+            window.cms.Flare26 &&
+            window.cms.Flare26.initDialogs &&
+            (installerHelpIcon || browserHelpIcon)
+        ) {
+            window.cms.Flare26.initDialogs();
+        }
+
+        // init stub attribution & event tracking for GA4
+        if (downloadButtons && downloadButtons.length > 0) {
+            if (StubAttributionConsent) {
+                // We cannot rely on DOM ready as this section is partially fetched
+                // This flow is scheduled for refactoring: https://github.com/mozmeao/springfield/issues/258
+                StubAttributionConsent.init();
+            }
+
+            for (let i = 0; i < downloadButtons.length; ++i) {
+                const downloadButton = downloadButtons[i];
+                downloadButton.addEventListener(
+                    'click',
+                    function (event) {
+                        TrackProductDownload.handleLink(event);
+                    },
+                    false
+                );
+            }
+        }
+
+        // Override click events for drill-down links.
+        getPartialTargetElement().addEventListener('click', function (event) {
+            const anchor = event.target.closest('a');
+            if (anchor && anchor.matches('.load-content-partial')) {
+                event.preventDefault();
+                fetchContent(anchor.href, true);
+            }
+        });
+    }
+
+    // A fetch helper since we use this in both the on click and popstate.
+    // pushState is a boolean so we avoid pushing state if triggered from popstate.
+    function fetchContent(url, pushState = false) {
+        fetch(url, {
+            // Signifies to backend to return partial HTML.
+            headers: { 'X-Requested-With': 'XMLHttpRequest' },
+            // Ignore what's cached and also don't cache this response.
+            // This is so we don't get full html pages when we expect partial html, or vice versa.
+            cache: 'no-store'
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then((html) => {
+                const partialTarget = getPartialTargetElement();
+                partialTarget.outerHTML = html;
+
+                // Re-attach listeners as we just replaced partialTarget
+                setUpPartialContentListeners();
+
+                if (pushState) {
+                    history.pushState({ path: url }, '', url);
+                }
+
+                const activeHeaders = document.querySelectorAll(
+                    '.c-step-name:not(.t-step-disabled)'
+                );
+                const targetHeader = activeHeaders[activeHeaders.length - 1];
+                if (targetHeader) {
+                    // if not already in view, scroll into view
+                    const rect = targetHeader.getBoundingClientRect();
+                    const isVisible =
+                        rect.top >= 0 && rect.top < window.innerHeight;
+                    if (!isVisible) {
+                        // avoid hiding content beneath fixed nav
+                        targetHeader.scrollIntoView({ block: 'center' });
+                    }
+                    // .focus() scroll is buggy
+                    targetHeader.focus({ preventScroll: true });
+                }
+            })
+            .catch((error) => {
+                throw new Error(
+                    'There was a problem with the fetch operation:',
+                    error
+                );
+            });
+    }
+
+    function onLoad() {
+        setUpPartialContentListeners();
+
+        // Add popstate listener so we return partial HTML with browser back button.
+        window.addEventListener('popstate', function (event) {
+            if (!event.state) {
+                return;
+            }
+            fetchContent(event.state.path, false);
+        });
+
+        // Ensure initial state is set up when the page loads so root page is in popstate.
+        window.addEventListener('DOMContentLoaded', () => {
+            const url = window.location.href;
+            history.replaceState({ path: url }, '', url);
+        });
+    }
+
+    Mozilla.run(onLoad);
+})(window.Mozilla);
