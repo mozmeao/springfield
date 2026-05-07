ca9e21e9c4d17a24424efb6f803aac74beedb879
Luciano Ratamero <luciano@ratamero.com>
2026-05-07 18:30:45 -0300

    Add visual regression tests for heading variants

---
+++ tests/playwright/specs/visual-regression/heading-2026.spec.js
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+const openPage = require('../../scripts/open-page');
+const { test } = require('@playwright/test');
+const { patternLibraryURL, expectComponentScreenshot } = require('./helpers');
+const url = `${patternLibraryURL}/heading/heading_2026.html`;
+
+test.describe(
+    `Heading 2026`,
+    {
+        tag: '@visual-regression'
+    },
+    () => {
+        test.beforeEach(async ({ page, browserName }) => {
+            await openPage(url, page, browserName);
+        });
+
+        test('heading with superheading and subheading', async ({ page }) => {
+            await expectComponentScreenshot(page, 'heading-2026');
+        });
+    }
+);
