2cdfea051631212c7a4f54597bf711425c0de740
maureenlholland <19650432+maureenlholland@users.noreply.github.com>
2026-05-13 15:46:17 +0100

    Reorg and update comment for default download attribution init JS

--- media/js/base/download-attribution/download-attribution-init.es6.js
+++ media/js/base/download-attribution/download-attribution-init.es6.js
@@ -6,10 +6,9 @@
 
 import DownloadAttribution from './download-attribution.es6';
 
-DownloadAttribution.applyAttributionDataToLinks();
-
-// We always want to refresh the essential data
-// to avoid an outdated download experience
-// If there's new essential data, update
-// If there's no essential data, remove
+// Refresh the essential data to avoid an outdated download experience
+// - If there's new essential data, update
+// - If there's no essential data, remove
 DownloadAttribution.initEssential();
+
+DownloadAttribution.applyAttributionDataToLinks();
