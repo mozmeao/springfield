203093d3cfe71079d52ca5f99130a413befe996b
luciano ratamero <luciano@ratamero.com>
2026-06-08 10:08:16 -0300

    WT-1262: Custom logo animation (#1477)
    * Wrap kick page selectors with .flare26-kick-page
    
    * Add option to flare26-header to render a custom logo
    
    * Render custom kick page logo if body class is correct
    
    * Normalize sizes for custom kick page logo
    
    * Add js to play custom kick page logo animation on hover
    
    * Add extra JS field to Free Form Page
    
    * Fix Two Column Cards block heading tag and sizing
    
    * Update screenshots
    
    ---------
    
    Co-authored-by: Mariana Bedran Lesche <maribedran@gmail.com>

---
+++ media/js/cms/pages/flare26-kick-page.es6.js
@@ -0,0 +1,57 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+function isAlphaWebmUnsupported() {
+    const ua = navigator.userAgent;
+    if (/iPad|iPhone|iPod/.test(ua)) return true;
+    if (/Macintosh/.test(ua) && navigator.maxTouchPoints > 1) return true;
+    return /^((?!chrome|android|crios|fxios).)*safari/i.test(ua);
+}
+
+const setupKickPage = () => {
+    if (!document.querySelector('.flare26-kick-page')) return;
+    if (isAlphaWebmUnsupported()) return;
+
+    const logoLink = document.querySelector('.fl-logo-fx');
+
+    if (!logoLink) return;
+
+    const existingLogo = logoLink.querySelector('img');
+    if (existingLogo) existingLogo.remove();
+
+    const wrapper = document.createElement('div');
+    wrapper.className = 'fl-video';
+
+    const video = document.createElement('video');
+    video.muted = true;
+    video.poster = '/media/img/logos/firefox/logo-word-hor-white-2026.svg';
+
+    const source = document.createElement('source');
+    source.src = 'https://assets.mozilla.net/wc/logo-1-alpha.webm';
+    source.type = 'video/webm';
+
+    video.appendChild(source);
+    wrapper.appendChild(video);
+    logoLink.appendChild(wrapper);
+
+    video.addEventListener('mouseover', () => {
+        video.play();
+    });
+    video.addEventListener('mouseout', () => {
+        video.pause();
+        video.currentTime = 0;
+    });
+
+    // play and pause to trigger the first frame to be loaded
+    video.play();
+    video.pause();
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', setupKickPage);
+} else {
+    setupKickPage();
+}
--- springfield/cms/models/pages.py
+++ springfield/cms/models/pages.py
@@ -65,7 +65,6 @@ from springfield.cms.blocks import (
     SlidingCarouselBlock,
     SubscriptionBlock,
     TopicListBlock,
-    TwoColumnCardsBlock,
     VideoBlock,
     validate_animation_url,
 )
@@ -891,7 +890,6 @@ def _get_freeform_page_blocks_2026(allow_uitour=True, allow_kit_intro=False):
         ("mobile_store_qr_code", MobileStoreQRCodeBlock(group="Media")),
         ("banner", BannerBlock(allow_uitour=allow_uitour, group="Banners")),
         ("topic_list", TopicListBlock(allow_uitour=allow_uitour, group="Main")),
-        ("two_column_cards", TwoColumnCardsBlock(allow_uitour=allow_uitour, group="Main")),
         ("line_cards", LineCardsBlock(allow_uitour=allow_uitour, template="cms/blocks/sections/line-cards-section.html", group="Main")),
         ("button_row", ButtonRowBlock(allow_uitour=allow_uitour, group="Main")),
         ("kit_banner", KitBannerBlock(allow_uitour=allow_uitour, group="Banners")),
@@ -988,6 +986,12 @@ class FreeFormPage2026(PromotedPageMixin, UTMParamsMixin, QRCodeFloatingSnippetM
             "The page will also inject <this>.css, so ensure that exists before using this field."
         ),
     )
+    extra_js = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name="Extra JS",
+        help_text=("Additional JavaScript file to include for this page. Use the static bundle name (without the .js extension)."),
+    )
 
     content_panels = AbstractSpringfieldCMSPage.content_panels + [
         FieldPanel("upper_content"),
@@ -1000,6 +1004,7 @@ class FreeFormPage2026(PromotedPageMixin, UTMParamsMixin, QRCodeFloatingSnippetM
                 InlinePanel("pencil_banner_placements", label="Pencil Banners"),
                 *QRCodeFloatingSnippetMixin.floating_qr_panels,
                 FieldPanel("body_class"),
+                FieldPanel("extra_js"),
             ],
             heading="Page Options",
         ),
