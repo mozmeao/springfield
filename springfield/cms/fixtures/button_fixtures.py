e282fe1bb58388f886aa3491bc3052c3dba24ed6
luciano ratamero <luciano@ratamero.com>
2026-07-06 10:06:09 -0300

    WT-1392: QR code modal (#1577)
    * Add QR code helper to make eyes rounder and add center logo
    
    * Add QRCodeModalButton block
    
    * Add fixtures and tests
    
    * Implement eye drawer for rounder QR code instead of editing the resulting image
    
    ---------
    
    Co-authored-by: Mariana Bedran Lesche <maribedran@gmail.com>

--- springfield/cms/fixtures/button_fixtures.py
+++ springfield/cms/fixtures/button_fixtures.py
@@ -347,6 +347,23 @@ def get_button_variants(full=False) -> dict[str, dict]:
             },
             "id": "fb000001-0000-0000-0000-000000000004",
         },
+        "qr_code_modal": {
+            "type": "qr_code_modal_button",
+            "value": {
+                "settings": {
+                    "theme": "",
+                    "icon": "device-mobile",
+                    "icon_position": "left",
+                    "analytics_id": "qrmodal1-0000-0000-0000-000000000001",
+                },
+                "pretranslated_label": None,
+                "custom_label": "Get Firefox on your phone",
+                "url": "https://www.mozilla.org/firefox/mobile/",
+                "heading": "Get Firefox on your phone",
+                "content": "Take Firefox with you.",
+            },
+            "id": "qrmodal1-0000-0000-0000-000000000002",
+        },
     }
     if full:
         index_page = get_flare_blocks_docs_page()
@@ -672,6 +689,27 @@ def get_button_blocks() -> list[dict]:
                 ],
             },
             "id": "focusintr-0000-0000-0000-000000000001",
+        },
+        {
+            "type": "intro",
+            "value": {
+                "settings": {"layout": "vertical", "slim": False, "anchor_id": ""},
+                "media": [],
+                "heading": {
+                    "superheading_text": "",
+                    "heading_text": '<p data-block-key="qrmodal01h">QR Code Modal Button</p>',
+                    "subheading_text": '<p data-block-key="qrmodal01s">Opens a modal with a QR code and Firefox logo. '
+                    "Editors configure the URL to encode, the modal heading, and an optional caption.</p>",
+                },
+                "content": [
+                    {
+                        "type": "buttons",
+                        "id": "qrmodal01-0000-0000-0000-000000000002",
+                        "value": [buttons["qr_code_modal"]],
+                    },
+                ],
+            },
+            "id": "qrmodal01-0000-0000-0000-000000000001",
         },
         {
             "type": "intro",
