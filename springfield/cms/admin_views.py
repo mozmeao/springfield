c8e780cbfc41a8ac4226a9cdea4c67b3f738fa44
Mariana Bedran Lesche <maribedran@gmail.com>
2026-08-06 14:27:59 -0300

    feat: repoint BlogArticlePage.tags to BlogTag

--- springfield/cms/admin_views.py
+++ springfield/cms/admin_views.py
@@ -5,8 +5,6 @@
 
 from django.http import JsonResponse
 
-from django.http import JsonResponse
-
 from wagtail.admin.views.pages.listing import IndexView
 from wagtail.admin.views.tags import TAGS_AUTOCOMPLETE_LIMIT
 from wagtail.models import Locale
