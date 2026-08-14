85c1e855b7c393121672d9f0765a146942210898
Luciano Ratamero <luciano@ratamero.com>
2026-08-13 21:13:25 -0300

    Add optional author's title field to quote block, and blog article quote styles

--- springfield/cms/blocks.py
+++ springfield/cms/blocks.py
@@ -1960,6 +1960,11 @@ class QuoteBlock(blocks.StructBlock):
         label="Author",
         help_text="Optional attribution for the quote.",
     )
+    authors_title = blocks.CharBlock(
+        required=False,
+        label="Author's title",
+        help_text="Optional title for the author of the quote.",
+    )
 
     class Meta:
         label = "Quote"
