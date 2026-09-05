0c8beabaded212267437083fed43dfc3b0c57a9b
Dan Brown <dbrown@mozilla.com>
2026-09-04 17:10:29 -0700

    Add conditionals to Media + Content block

--- springfield/cms/blocks.py
+++ springfield/cms/blocks.py
@@ -1953,13 +1953,17 @@ class MediaContentSettings(blocks.StructBlock):
         inline_form=True,
         help_text="Narrow the media element",
     )
+    show_to = ConditionalDisplayBlock(
+        label="Show To",
+        help_text="Control which users can see this content block",
+    )
     remove_border_radius = blocks.BooleanBlock(required=False, default=False, help_text="Remove rounded borders from media.")
 
     class Meta:
         icon = "cog"
         collapsed = True
         label = "Settings"
-        label_format = "Media After: {media_after}"
+        label_format = "Media After: {media_after} - Show to: {show_to}"
         form_classname = "compact-form struct-block"
 
 
