a1951b695275a729280c07347fb295f8a836c3c7
Mariana Bedran Lesche <maribedran@gmail.com>
2026-09-15 19:39:49 -0300

    Update migration number

--- springfield/cms/migrations/0154_springfieldimage_is_decorative.py
+++ springfield/cms/migrations/0154_springfieldimage_is_decorative.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("cms", "0152_merge_20260910_1400"),
+        ("cms", "0153_create_welsh_locale"),
     ]
 
     operations = [
