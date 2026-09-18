1a36b20d0d065413ea86692f14bd6d02f7a81a98
Mariana Bedran Lesche <maribedran@gmail.com>
2026-09-18 13:20:20 -0300

    Update migration numbers

--- springfield/cms/migrations/0155_contactpage_document_download_and_more.py
+++ springfield/cms/migrations/0155_contactpage_document_download_and_more.py
@@ -10,7 +10,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("cms", "0153_create_welsh_locale"),
+        ("cms", "0154_move_blog_models_to_blog_app"),
         ("wagtaildocs", "0014_alter_document_file_size"),
     ]
 
